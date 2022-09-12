2a53477cb6034b8cfe8773aa6ff1ca8ff725330d
Gabriel Henrique Fiszczuk Brandeburski <brandeburskigabrielhenrique@gmail.com>
2022-09-12 20:43:37 -0300

    Limpeza de código e alteração no footer

--- js/main.js
+++ js/main.js
@@ -1,32 +1,24 @@
 window.onscroll = function() {progressoScroll()};
 
 function progressoScroll() {
-  var scrollJanela = document.body.scrollTop || document.documentElement.scrollTop;
-  var altura = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var progresso = (scrollJanela / altura) * 100;
+  let scrollJanela = document.body.scrollTop || document.documentElement.scrollTop;
+  let altura = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let progresso = (scrollJanela / altura) * 100;
   document.getElementById("barraProg").style.width = progresso + "%";
 } 
 
-// Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("modalFormulario");
+let botao = document.getElementById("botaoContato");
+let span = document.getElementsByClassName("fechar")[0];
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
+botao.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
